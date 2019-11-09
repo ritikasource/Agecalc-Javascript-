490a4056c233e079face35bbdb50a4eacb770894
ritikasource <ritika99.work@gmail.com>
2019-11-09 18:39:26 +0530

    Added javascript file

---
+++ calc.js
@@ -0,0 +1,9 @@
+var age= prompt("How old are you?(in years)")
+if (age==0)
+	alert("Your journey has just started!")
+else
+{
+var nage= age*365;
+
+alert("Your age " + age + " is roughly " + nage + " " + "days");
+}
